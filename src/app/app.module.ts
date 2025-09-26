c5507ed146afed483fe63216a751cf78ef900db7
unknown <camilo.quintero-r@labinfo.is.escuelaing.edu.co>
2025-09-26 11:31:10 -0500

    Estrucutra base de datos login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '../app/modules/auth/auth.module';
 
 @Module({
 	imports: [
