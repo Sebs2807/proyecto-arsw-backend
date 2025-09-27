1c3a832f7d0e3713b0c1f2e2d2fcfb0d6cb122fd
Santiago Díaz <sadiro_0511@outlook.com>
2025-09-26 23:32:12 -0500

    Fix in callback URL and main.ts for api versions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { VersioningType } from '@nestjs/common';
 import * as fs from 'fs';
 
 async function bootstrap() {
@@ -7,7 +8,13 @@ async function bootstrap() {
     key: fs.readFileSync('./certs/synapse+1-key.pem'),
     cert: fs.readFileSync('./certs/synapse+1.pem'),
   };
+
   const app = await NestFactory.create(AppModule, { httpsOptions });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
