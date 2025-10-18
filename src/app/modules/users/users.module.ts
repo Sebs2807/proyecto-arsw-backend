a739d5b387a164d60c8f0e724797f3b97ef76ea4
Santiago Díaz <sadiro_0511@outlook.com>
2025-10-17 22:39:54 -0500

    Users endpoint paginated

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,15 +1,21 @@
+// src/modules/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../../../database/entities/user.entity';
 import { UsersDBService } from '../../../database/dbservices/users.dbservice';
 import { UsersService } from '../users/users.service';
+import { UsersController } from '../users/users.controller';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
-    JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '1h' } }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
+  controllers: [UsersController],
   providers: [UsersDBService, UsersService],
   exports: [UsersDBService, UsersService],
 })
